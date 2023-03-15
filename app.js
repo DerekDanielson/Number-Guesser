ef1d41bcdbc11d5165cb3524b8ec79af87bfbcb4
Derek <derekdanielson84@gmail.com>
2023-03-15 10:27:35 -0600

    Added game lose code

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ maxNum.textContent = max;
 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
+    // Turn guess into number from string
     let guess = parseInt(guessInput.value);
 
     // Validate
@@ -36,16 +37,43 @@ guessBtn.addEventListener('click', function(){
 
     // Check if won
     if(guess === winningNum){
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct`, 'green');
+        // Game Over - Won
+        gameOver(true, `${winningNum} is correct`);
     } else {
+        // Wrong number
+        guessesLeft -= 1;
 
+        if(guessesLeft === 0){
+            // Game Over - lost
+            gameOver(false, `Game Over, ${winningNum} was correct answer`);
+        } else {
+            // Game continues - answer wrong
+            // Change border color
+            guessInput.style.borderColor = 'red';
+
+            // Clear input
+            guessInput.value = '';
+            // Set message
+            setMessage(msg);
+        }
     }
 });
+
+// Game Over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set message
+    setMessage(`${winningNum} is correct`, color);
+}
+
 
 // Set message
 function setMessage(msg, color){
